121af10b56be23690fff6b0be1ebdbb36830020b
victorvallet <victorvallet8@gmail.com>
2020-05-05 10:18:18 +0100

    Added array of students and counter

--- directory.rb
+++ directory.rb
@@ -1,18 +1,22 @@
-#declare viariable student_count
-student_count = 11
-#first we print the list of vilains
+#Let's put all students into an Array
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+]
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+#puts each student from the students array
+students.each do |student|
+  puts student
+end
 #finally we print the total number of students
-puts `Overall, we have #{student_count} great students`
+puts "Overall, we have " + students.count.to_s + " great students"
