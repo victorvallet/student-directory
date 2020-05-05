db5b808c47cd60884135fb3e63d0d7f0eb3112cb
victorvallet <victorvallet8@gmail.com>
2020-05-05 11:26:51 +0100

    Created a copy of directory.rb

---
+++ directory_upgraded.rb
@@ -0,0 +1,30 @@
+#Let's define a new method to ask the user the names to put in our array
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  #start our while loop
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+students = input_students
+print_header
+print(students)
+print_footer(students)
